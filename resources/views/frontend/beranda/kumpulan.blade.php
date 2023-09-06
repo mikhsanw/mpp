7d6755ab6b32e126c0588b26c2609a109a1ffee0
herimaulana <herimaulana85@gmail.com>
2023-09-06 15:04:27 +0700

    Membuang hal yang gak perlu

--- resources/views/frontend/beranda/kumpulan.blade.php
+++ resources/views/frontend/beranda/kumpulan.blade.php
@@ -32,38 +32,7 @@
                 <input type="text" name="search" id="search" class="form-control" placeholder="Cari berdasarkan nama unit layanan" />
             </div>
             <div class="row g-4" id="data-instansi">
-                {{-- @foreach ($data as $item)
-                <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
-                    <div class="team-item">
-                        <img class="img-fluid" src="{{asset($item->file->url_stream)}}" alt="" style="width: 315px; height: 355px; object-fit:contain; display: block; margin-left: auto; margin-right: auto;">
-                        <div class="d-flex">
-                            <a href="{{url('company/instansi-detail', $item->id)}}">
-                                <div class="flex-shrink-0 btn-square bg-primary" style="width: 90px; height: 90px;">
-                                <i class="fa fa-2x fa-share text-white"></i>
-                            </div></a>
-                            <div class="position-relative overflow-hidden bg-light d-flex flex-column justify-content-center w-100 ps-4"
-                                style="height: 90px;">
-                                <h5>{{$item->nama}}</h5>
-                                <span class="text-primary">{{$item->alamat}}</span>
-                                <div class="team-social d-flex">
-                                    <div class="d-flex" style="align-items: center; margin-right: 15px;">
-                                        <a class="btn btn-square btn-dark rounded-circle mx-1" href="">
-                                            <i class="fa fa-desktop"></i>
-                                        </a>
-                                        <h6 class="text-white" style="font-size: 11pt; margin-top:8px; margin-left: 5px;">1 Loket</h6>
-                                    </div>
-                                    <div class="d-flex" style="align-items: center;">
-                                        <a class="btn btn-square btn-dark rounded-circle mx-1" href="">
-                                            <i class="fa fa-bookmark"></i>
-                                        </a>
-                                        <h6 class="text-white" style="font-size: 11pt; margin-top:8px; margin-left: 5px;">{{App\Model\Layanan::whereInstansiId($item->id)->count()}} Pelayanan</h6>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                @endforeach --}}
+                
             </div>
             <div class="pagination pagination-lg mt-5 mx-auto d-block">
                 {{ $data->links() }}
